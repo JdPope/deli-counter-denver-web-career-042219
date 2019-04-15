2bd4ae1b1221b1bd94f935c18561c49237b44142
Jessica Pope <jessica.pope.co@gmail.com>
2019-04-15 16:05:52 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,8 @@
 def line(katz_deli)
-  current_place = []
-  current_place.first("The Line is currently: "
+  
   if katz_deli.length>0 
     katz_deli.each_with_index do |name,index|
-    if katz_deli.length = 1 
-      current_place.push("The Line is currently: #{index}. #{name}")
+      "#{index}. #{name}"
     else current_place.push("#{index}. #{name}")
   end
   end
