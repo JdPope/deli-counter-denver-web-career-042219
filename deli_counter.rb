bacbfc3d151016ab06faaba5a3eca6f0336195e9
Jessica Pope <jessica.pope.co@gmail.com>
2019-04-15 17:05:46 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,7 +20,9 @@ def now_serving(katz_deli)
   if katz_deli.length>0
     serving = katz_deli.shift()
     puts "currently serving #{serving}."
-  else 
+  else "There is nobody waiting to be served!"
+  end
+end
   
   
   
