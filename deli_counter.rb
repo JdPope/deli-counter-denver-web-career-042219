e0f526eb97f8cf6d1eb125970bd95109a1157a28
Jessica Pope <jessica.pope.co@gmail.com>
2019-04-15 16:58:27 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,6 +13,6 @@ end
 def take_a_number(katz_deli, name)
   katz_deli.push(name)
   katz_deli.each_with_index do |name,index|
-  puts "Welcome, #{name}. You are #{index+1} in line."
+  puts "Welcome, #{name}. You are number #{index+1} in line."
 end
 end
