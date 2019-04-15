9219c43c235462089d8713d6450c5bef462f1a25
Jessica Pope <jessica.pope.co@gmail.com>
2019-04-15 16:10:19 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ def line(katz_deli)
   if katz_deli.length>0
     current_line = "The Line is currently:"
     katz_deli.each_with_index do |name,index|
-     current_line += " #{index}. #{name}"
+     current_line += " #{index+1}. #{name}"
     end
 
   else puts "The line is currently empty."
