6a9e5287e849eb5d0e5978a51f9d11b5eb6aabf4
Jessica Pope <jessica.pope.co@gmail.com>
2019-04-15 17:02:07 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,6 +12,8 @@ end
 
 def take_a_number(katz_deli, name)
   katz_deli.push(name)
-  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+  def index = katz_deli.length
+  end
+  puts "Welcome, #{name}. You are number #{index} in line."
 end
 end
