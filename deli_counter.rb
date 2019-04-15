6a6b544e69090afc7e0f8f3ce0c21b7c212e7f85
Jessica Pope <jessica.pope.co@gmail.com>
2019-04-15 15:52:18 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ def line(katz_deli)
   if katz_deli.length>0 
   katz_deli.each_with_index do |name,index|
     current_place.push("#{index}. #{name}")
-    end
+    
   end
   else puts "The line is currently empty."
   end
